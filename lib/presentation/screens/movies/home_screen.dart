2c21831ab10e151cd0de2f8a0fa80e273d052a3e
Gerardo Estrella <g.serrato41@outlook.com>
2023-07-24 21:57:29 -0600

    first comit

---
+++ lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  static const String name = 'home_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
